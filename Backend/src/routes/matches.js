9c4d55d7abd78bb208463f9d993992fb89e09a2e
heisenberg1316 <rawatmukul5000@gmail.com>
2026-03-02 16:28:14 +0530

    implement matches POST and GET routes

---
+++ Backend/src/routes/matches.js
@@ -0,0 +1,55 @@
+import { Router } from "express";
+import { createMatchSchema, listMatchesQuerySchema } from "../validation/matches.js";
+import { db } from "../db/db.js";
+import { matches } from "../db/schema.js";
+import { getMatchStatus } from "../utils/match-status.js";
+import { desc } from "drizzle-orm";
+
+export const matchRouter = Router();
+
+matchRouter.get("/", async (req, res) => {
+    const parsed = listMatchesQuerySchema.safeParse(req.query);
+
+    const MAX_LIMIT = 100;
+
+    if(!parsed.success){
+        return res.status(400).json({error : "Invalid query", details : JSON.stringify(parsed.error)});
+    }
+
+    const limit = Math.min(parsed.data.limit  ?? 50, MAX_LIMIT);
+
+    try{
+        const data = await db.select().from(matches).orderBy((desc(matches.createdAt))).limit(limit);
+        return res.json({ data });
+    }
+    catch(e){
+        console.log("error is ", e);
+        return res.status(500).json({error : "Failed to list matches"});
+    }
+})
+
+matchRouter.post("/", async (req, res) => {
+    const parsed = createMatchSchema.safeParse(req.body);
+    const { data : { startTime, endTime, homeScore, awayScore }} = parsed;
+
+    if(!parsed.success){
+        return res.status(400).json({error : "Invalid payload", details : JSON.stringify(parsed.error)});
+    }
+
+    try{
+        const [event] = await db.insert(matches).values({
+            ...parsed.data,
+            startTime : new Date(startTime),
+            endTime : new Date(endTime),
+            homeScore : homeScore ?? 0,
+            awayScore : awayScore ?? 0,
+            status : getMatchStatus(startTime, endTime),
+        }).returning();
+
+        return res.status(201).json({ data : event });
+    }
+    catch(e){
+        return res.status(500).json({error : "Failed to create match", details : JSON.stringify(e)});
+    }
+})
+
