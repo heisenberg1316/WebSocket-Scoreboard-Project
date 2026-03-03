09bc6ad7837f161bed0517e4ba07df778340a11a
heisenberg1316 <rawatmukul5000@gmail.com>
2026-03-03 13:13:39 +0530

    implement ping pong

--- Backend/src/ws/server.js
+++ Backend/src/ws/server.js
@@ -1,4 +1,4 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 function sendJson(socket, payload) {
     if(socket.readyState !== WebSocket.OPEN) return;
@@ -15,16 +15,32 @@ function broadcast(wss, payload) {
 }
 
 export function attachWebSocketServer(server) {
-    const wss = new WebSocketServer({ server, path : "/ws", maxPayload : 1024 * 1024});
+    const wss = new WebSocketServer({ server, path: '/ws', maxPayload: 1024 * 1024 });
 
-    wss.on("connection", (socket) => {
-        sendJson(socket, { type : "welcome" });
-        socket.on("error", console.error);
+    wss.on('connection', async (socket, req) => {
+        socket.isAlive = true;
+        socket.on('pong', () => { socket.isAlive = true; });
+
+        socket.subscriptions = new Set();
+
+        sendJson(socket, { type: 'welcome' });
+
+        socket.on('error', console.error);
     });
 
-    function broadcastMatchCreated(match){
-        broadcast(wss, { type : "match_created", data : match});
+    const interval = setInterval(() => {
+        wss.clients.forEach((ws) => {
+            if (ws.isAlive === false) return ws.terminate();
+
+            ws.isAlive = false;
+            ws.ping();
+        })}, 30000);
+
+    wss.on('close', () => clearInterval(interval));
+
+    function broadcastMatchCreated(match) {
+        broadcast(wss, { type: 'match_created', data: match });
     }
 
-    return { broadcastMatchCreated }
+    return { broadcastMatchCreated };
 }
