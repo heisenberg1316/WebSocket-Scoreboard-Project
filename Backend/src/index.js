534a19165c84d3d5e02566a6f551462519323f81
heisenberg1316 <rawatmukul5000@gmail.com>
2026-03-06 20:03:08 +0530

    add frontend project

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,15 +4,27 @@ import http from "http";
 import { attachWebSocketServer } from "./ws/server.js";
 import { securityMiddleware } from "./arcjet.js";
 import { commentaryRouter } from "./routes/commentary.js";
+import cors from "cors";
 
 const PORT = Number(process.env.PORT || 8000);
 const HOST = process.env.HOST || "0.0.0.0";
 
 const app = express();
 const server = http.createServer(app);
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://your-frontend-domain.com"
+];
+
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(securityMiddleware());
+
+//cors issue remaning i think
 
 app.use("/matches", matchRouter); 
 app.use("/matches/:id/commentary", commentaryRouter)
@@ -24,7 +36,6 @@ app.get("/", (req, res) => {
 const { broadcastMatchCreated, broadcastCommentary } = attachWebSocketServer(server);
 app.locals.broadcastMatchCreated = broadcastMatchCreated;
 app.locals.broadcastCommentary = broadcastCommentary;
-
 
 
 server.listen(PORT, HOST, () => {
